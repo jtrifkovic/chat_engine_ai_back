98aca79c42fd29066bc57cff684f283aeaadb349
jtrifkovic <jowantrifkovic@gmail.com>
2023-03-16 13:11:26 +0100

    bug fixed

--- index.js
+++ index.js
@@ -20,6 +20,8 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }))
 app.use(bodyParser.urlencoded({ limit: "30mb" , extended: true }))
 app.use(cors())
 
+
+
 /* OPEN AI CONFIGURATION */
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY
@@ -28,6 +30,8 @@ export const openai = new OpenAIApi(configuration);
 
 /* ROUTES */
 app.use("/openai", openAiRoutes)
+
+
 
 /* SERVER SETUP */
 const PORT = process.env.PORT || 9000
